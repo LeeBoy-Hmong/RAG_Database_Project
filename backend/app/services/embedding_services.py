20b807d4a426a7422c461237a5250f3284fa6523
Michael Lee <michael1998lee@outlook.com>
2025-12-09 00:10:51 -0600

    Addition of two different embedding models for future test

--- backend/app/services/embedding_services.py
+++ backend/app/services/embedding_services.py
@@ -1,6 +1,16 @@
 from sentence_transformers import SentenceTransformer
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
+model = SentenceTransformer("all-MiniLM-L6-v2") # Dimension 384
 
-def embedded_text(text: str):
+def embed_text(text: str):
     return model.encode(text)
+
+model2 = SentenceTransformer("multi-qa-MiniLM-L6-cos-v1")  # Dimension 384
+
+def embed_text2(text: str):
+    return model2.encode(text)
+
+model3 = SentenceTransformer("all-MPNet-base-v2")  # Dimension 768
+
+def embed_txt3(text: str):
+    return model3.encode(text)
