1f604fe5bd7fe3f591786eeb1aa4c2241435e3ca
Michael Lee <michael1998lee@outlook.com>
2025-12-09 16:08:18 -0600

    Loader test - complete

--- backend/app/ingestion/loader.py
+++ backend/app/ingestion/loader.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 '''Load all text files from the docs file path'''
-def load_documents(dir_path="docs"):
+def load_documents(dir_path="backend/app/ingestion/docs"):  # 
     print(f"Documents are loading from {dir_path}...")
 # Check if documents in the particular directory exist
     if not os.path.exists(dir_path):
@@ -14,13 +14,23 @@ def load_documents(dir_path="docs"):
     doc_loader = DirectoryLoader(
         path = dir_path,
         glob = "*.txt",
-        loader_cls = TextLoader
+        loader_cls = TextLoader,
+        loader_kwargs={
+            "encoding": "UTF-8",  # Needs to be passed in - Windows Python is not utilizing the correct encoder. 
+        },
     )
 
     documents = doc_loader.load()
 # Raise error is the document (.txt files) do not exist
     if len(documents) == 0:
         raise FileNotFoundError(f"Can not find the particular file(s) you are looking for. Please ensure files exist.")
+    
+    for i, doc in enumerate(documents[:2]):
+        print(f"\nDocument {i+1}:")
+        print(f"Source: {doc.metadata['source']}")
+        print(f"Content length: {len(doc.page_content)}")
+        print(f"Content Preview: {doc.page_content[:100]}")
+        print(f"Metadata: {doc.metadata}")
     
     return documents
 
