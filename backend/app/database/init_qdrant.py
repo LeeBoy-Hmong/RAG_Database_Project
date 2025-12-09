26fc316d287b98f2e13fecbdf0adfe31420b4fa8
Michael Lee <michael1998lee@outlook.com>
2025-12-09 00:30:32 -0600

    Database Model additions of Model 2 & 3 from Service directory

--- backend/app/database/init_qdrant.py
+++ backend/app/database/init_qdrant.py
@@ -1,5 +1,5 @@
 from qdrant_client import QdrantClient, models
-from app.services.embedding_services import model
+from app.services.embedding_services import model, model2, model3
 
 def init_qdrant():
     qdrant = QdrantClient(url="http://localhost:6333")
@@ -9,3 +9,19 @@ def init_qdrant():
         collection_name= "document-vectors",
         vectors_config= models.VectorParams(size=embedding_dim, distance=models.Distance.COSINE)
     )
+
+'''def init_qdrant2():
+    qdrant = QdrantClient("http://localmachine:6333")
+    embedding_dim = model2.get_sentence_embedding_dimension()
+    qdrant.recreate_collection(
+        collection_name ="new-document",
+        vectors_config = models.VectorParams(size=embedding_dim, distance=models.Distance.COSINE)
+    )'''
+
+'''def init_qdrant3():
+    qdrant = QdrantClient("https://localhost:6333")
+    embedding_dim = model3.get_sentence_embedding_dimension()
+    qdrant.recreate_collection(
+        collection_name="document3-collection",
+        vectors_config = models.VectorParams(size=embedding_dim, distance=models.Distance.COSINE)
+    )'''
